6bf3cfb896745ab5b501a74adb3532e3cd5fb6e0
JanGaud <JanisGaudreault.jg@gmail.com>
2023-08-17 14:10:46 -0400

    Fin de la base de la Home page

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="About">
+      
+    </div>
+  )
+}
+
+export default About
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import {hiArrowNarrowRight} from "react-icons/hi";
 
 const Home = () => {
   return (
-  <section name='home' className="w-full h-screen bg-[--russian-violet]">
-    {/* Container */}
-
-  </section>
+    <section name="home" className="w-full h-screen bg-[--russian-violet]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-[--orange-peel]">Bonjour, mon nom est</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#e7e7e7]">
+          Janis Gaudreault
+        </h1>
+        <h2 className="text-3xl sm:text-6xl text-[#ff840092]">
+          Je suis une développeuse Web Full Stack.
+        </h2>
+        <p className="text-[#e7e7e7] py-4 max-w-[800px]">
+          "Bonjour, je suis Janis Gaudreault, développeuse full stack
+          spécialisée dans le web. Mon expertise englobe le développement
+          complet d'applications web, du front-end au back-end. Passionnée par
+          le mariage entre le code et le design, j'apprécie particulièrement
+          créer des interfaces esthétiques et conviviales.
+        </p>
+        <div>
+          <button className="text-gray-300 border-2 p-3 my-2 hover:border-[--amethyst] duration-300">
+            Voir mes Travaux
+          </button>
+        </div>
+      </div>
+    </section>
   );
 }
 
