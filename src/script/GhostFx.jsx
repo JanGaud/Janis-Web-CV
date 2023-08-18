da2a8a98152f32549e3ce18854fb631c76ebd332
JanGaud <JanisGaudreault.jg@gmail.com>
2023-08-18 13:18:00 -0400

    Les hovers sont réparés, j'ai ajusté l'animation

--- src/script/GhostFx.jsx
+++ src/script/GhostFx.jsx
@@ -2,32 +2,27 @@ import React, { useEffect } from "react";
 
 const GhostEffect = () => {
   useEffect(() => {
-const handleScroll = () => {
-  document.querySelectorAll(".ghostFx").forEach((container) => {
-    const { top, bottom } = container.getBoundingClientRect();
+    const handleScroll = () => {
+      document.querySelectorAll(".ghostFx").forEach((container) => {
+        const { top, bottom } = container.getBoundingClientRect();
 
-    // Check if the container is at least halfway in the viewport
-    if (
-      top + container.offsetHeight / 2 < window.innerHeight &&
-      bottom - container.offsetHeight / 2 > 0
-    ) {
-      Array.from(container.children).forEach((child) => {
-        child.style.opacity = "1";
-        child.style.visibility = "visible";
-        child.style.maxHeight = "1000px"; // Choose a value that ensures your content will be visible. Adjust if needed.
-        child.style.transform = "translateY(0%)";
+        // Check if the container is at least halfway in the viewport
+        if (
+          top + container.offsetHeight / 2 < window.innerHeight &&
+          bottom - container.offsetHeight / 2 > 0
+        ) {
+          Array.from(container.children).forEach((child) => {
+            child.style.opacity = "1";
+            child.style.transform = "translateX(0%)"; // Place them in their original position
+          });
+        } else {
+          Array.from(container.children).forEach((child) => {
+            child.style.opacity = "0";
+            child.style.transform = "translateX(-100%)"; // Slide them out to the left
+          });
+        }
       });
-    } else {
-      Array.from(container.children).forEach((child) => {
-        child.style.opacity = "0";
-        child.style.visibility = "hidden";
-        child.style.maxHeight = "0";
-        child.style.transform = "translateY(100%)";
-      });
-    }
-  });
-};
-
+    };
 
     window.addEventListener("scroll", handleScroll);
     handleScroll(); // Trigger immediately on mount
