3d3d1b06af1e4b35e067c79373a8b9804e7c60cb
JanGaud <JanisGaudreault.jg@gmail.com>
2023-08-21 18:35:52 -0400

    Liens vers la page chico fait...

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import VinStock from "../assets/projects/vinstock/VinStock.png";
 import Chico from "../assets/projects/chico/Chico.jpg";
 import Maisonneuve from "../assets/projects/maisonneuve/Maisonneuve.png";
+import Unavailable from "../assets/unavailable.png";
 
 const Work = () => {
   return (
@@ -16,7 +19,7 @@ const Work = () => {
           </p>
         </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 mb-20">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-6 mb-20">
           {/* Card */}
           <div
             style={{ backgroundImage: `url(${VinStock})` }}
@@ -31,7 +34,7 @@ const Work = () => {
               </div>
               <div className="pt-8 text-center">
                 <a href="https://www.vinstock.ca/login" target="_blank">
-                  <button className="text-gray-300 border-2 p-3 hover:border-[--amethyst] duration-300">
+                  <button className="text-gray-300 border-2 p-2 hover:border-[--amethyst] duration-300">
                     Visiter le site!
                   </button>
                 </a>
@@ -51,11 +54,11 @@ const Work = () => {
                 <small>Interface admin</small>
               </div>
               <div className="pt-8 text-center">
-                <a href="#">
-                  <button className="text-gray-300 border-2 p-3 hover:border-[--amethyst] duration-300">
+                <Link to="/Chico">
+                  <button className="text-gray-300 border-2 p-2 hover:border-[--amethyst] duration-300">
                     Voir plus!
                   </button>
-                </a>
+                </Link>
               </div>
             </div>
           </div>
@@ -73,7 +76,7 @@ const Work = () => {
               </div>
               <div className="pt-8 text-center">
                 <a href="#">
-                  <button className="text-gray-300 border-2 p-3 hover:border-[--amethyst] duration-300">
+                  <button className="text-gray-300 border-2 p-2 hover:border-[--amethyst] duration-300">
                     Voir plus!
                   </button>
                 </a>
@@ -82,55 +85,55 @@ const Work = () => {
           </div>
           {/* Card */}
           <div
-            style={{ backgroundImage: `url(${VinStock})` }}
+            style={{ backgroundImage: `url(${Unavailable})` }}
             className="z-10 group container rounded-md flex justify-center items-center mx-auto content-div p-2"
           >
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                React JS application
+                Bientôt disponible
               </span>
               <div className="pt-8 text-center">
-                <a href="#">
-                  <button className="text-gray-300 border-2 p-3 hover:border-[--amethyst] duration-300">
+                {/* <a href="#">
+                  <button className="text-gray-300 border-2 p-2 hover:border-[--amethyst] duration-300">
                     Voir plus!
                   </button>
-                </a>
+                </a> */}
               </div>
             </div>
           </div>
           {/* Card */}
           <div
-            style={{ backgroundImage: `url(${VinStock})` }}
+            style={{ backgroundImage: `url(${Unavailable})` }}
             className="z-10 group container rounded-md flex justify-center items-center mx-auto content-div p-2"
           >
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                React JS application
+                Bientôt disponible
               </span>
               <div className="pt-8 text-center">
-                <a href="#">
-                  <button className="text-gray-300 border-2 p-3 hover:border-[--amethyst] duration-300">
+                {/* <a href="#">
+                  <button className="text-gray-300 border-2 p-2 hover:border-[--amethyst] duration-300">
                     Voir plus!
                   </button>
-                </a>
+                </a> */}
               </div>
             </div>
           </div>
           {/* Card */}
           <div
-            style={{ backgroundImage: `url(${VinStock})` }}
+            style={{ backgroundImage: `url(${Unavailable})` }}
             className="z-10 group container rounded-md flex justify-center items-center mx-auto content-div p-2"
           >
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                React JS application
+                Bientôt disponible
               </span>
               <div className="pt-8 text-center">
-                <a href="#">
-                  <button className="text-gray-300 border-2 p-3 hover:border-[--amethyst] duration-300">
+                {/* <a href="#">
+                  <button className="text-gray-300 border-2 p-2 hover:border-[--amethyst] duration-300">
                     Voir plus!
                   </button>
-                </a>
+                </a> */}
               </div>
             </div>
           </div>
